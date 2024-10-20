e53dc8fbb44b3c5f4a8bfd70c1bc8af488814166
Fedi-1 <feditriki05@gmail.com>
2024-10-20 22:06:32 +0100

    a

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,31 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import $ from 'jquery';
-
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.css'
+  styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent {
-  title(title: any) {
-    throw new Error('Method not implemented.');
-}}
-$("document").ready(() => {
-  $(".dropdown").hover(
-      function () {
-          const dropdown = $(this as HTMLElement); // 'this' refers to the current dropdown
-          dropdown.find('.dropdown-menu').stop(true, true).slideDown(400);
-          dropdown.toggleClass('open');
-      },
-      function() {
-          const dropdown = $(this as unknown as HTMLElement); // Use 'this' from outer scope
-          dropdown.find('.dropdown-menu').stop(true, true).slideUp(400);
-          dropdown.toggleClass('open');
-      }
-  );
-});
-
-
+export class NavBarComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    if (typeof window !== 'undefined') {
+      $(document).ready(() => {
+        $(".dropdown").hover(
+          function () {
+            const dropdown = $(this as HTMLElement); // 'this' refers to the current dropdown
+            dropdown.find('.dropdown-menu').stop(true, true).slideDown(400);
+            dropdown.toggleClass('open');
+          },
+          function () {
+            const dropdown = $(this as unknown as HTMLElement); // Use 'this' from outer scope
+            dropdown.find('.dropdown-menu').stop(true, true).slideUp(400);
+            dropdown.toggleClass('open');
+          }
+        );
+      });
+    }
+  }
+}
