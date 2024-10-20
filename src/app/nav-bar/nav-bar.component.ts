bc4c3753e4ec8fd3f5cd267995951bea6ef388bf
handerpool <nidhalnsibi39@gmail.com>
2024-10-20 22:01:23 +0100

    g

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import $ from 'jquery';
+
 
 @Component({
   selector: 'app-nav-bar',
@@ -8,5 +10,22 @@ import { Component } from '@angular/core';
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
+  title(title: any) {
+    throw new Error('Method not implemented.');
+}}
+$("document").ready(() => {
+  $(".dropdown").hover(
+      function () {
+          const dropdown = $(this as HTMLElement); // 'this' refers to the current dropdown
+          dropdown.find('.dropdown-menu').stop(true, true).slideDown(400);
+          dropdown.toggleClass('open');
+      },
+      function() {
+          const dropdown = $(this as unknown as HTMLElement); // Use 'this' from outer scope
+          dropdown.find('.dropdown-menu').stop(true, true).slideUp(400);
+          dropdown.toggleClass('open');
+      }
+  );
+});
 
-}
+
